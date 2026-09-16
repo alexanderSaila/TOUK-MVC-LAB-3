5b7344132260bb30554ecbd65305ff05cc955496
Alexander <alex05.12@outlook.com>
2026-09-16 10:42:59 +0200

    Initialize data fields and methods in Controller

--- src/Controller.java
+++ src/Controller.java
@@ -2,4 +2,20 @@
 
 // This is a dummy Controller, so the View compiles
 public class Controller {
+
+    View view;
+    EncryptionEngine encryptionEngine;
+
+    public Controller(){
+        encryptionEngine = new EncryptionEngine(this);
+        view = new View(this);
+    }
+
+    public void encryptButtonClicked(){
+
+    }
+
+    public void decryptButtonClicked(){
+
+    }
 }
